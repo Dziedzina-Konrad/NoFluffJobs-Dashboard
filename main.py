f4501338e3ca64a91bb39dbbdc316f3ebaf57f2e
Konrad Dziedzina <dziedzina.konrad@gmail.com>
2023-09-17 14:13:20 +0200

    Completed func for scraping skills

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 from scrapper import NoFluffJobs
+
+nfj = NoFluffJobs()
+nfj.job_scrapper_manager(100)
+print(nfj.mandatory_skills)
+print(nfj.nice_to_have_skills)
+nfj.create_skills_df()
+nfj.skills_to_csv()
--- scrapper.py
+++ scrapper.py
@@ -17,16 +17,30 @@ class NoFluffJobs():
       self.job_pages = []
       for job in nfj_jobs:
          job_key = job["name"].strip() + "--" + job["title"].strip()
-         job_url = self.job_url + job["url"]
          self.job_pages.append([job_key, self.job_url + job["url"]])
+      self.mandatory_skills = []
+      self.nice_to_have_skills = []
 
    def get_job_skills(self, link):
-      soup = BeautifulSoup(requests.get(link).text)
+      """Get skills from nfj website that is passed as parametr link
+
+      Args:
+          link (string): url for website with job details
+
+      Returns:
+          array: Return 2 arrays, first with mandatory skills, second with optional skills.
+      """
+      soup = BeautifulSoup(requests.get(link).text, "html5lib")
       must_have_section = soup.find("section", class_="d-block")
       must_have_skills = []
-      for skill in must_have_section.find_all("li"):
-         must_have_skills.append(skill.text.strip())
-      
+      try:
+         for skill in must_have_section.find_all("li"):
+            must_have_skills.append(skill.text.strip())
+      except:
+         soup = BeautifulSoup(requests.get(link).text, "html5lib")
+         must_have_section = soup.find("section", class_="d-block")
+         for skill in must_have_section.find_all("li"):
+            must_have_skills.append(skill.text.strip())
       nice_to_have_skills = []
       try:
          nice_to_have_section = soup.find("section" , class_="d-block mt-3 ng-star-inserted")
@@ -34,5 +48,32 @@ class NoFluffJobs():
             nice_to_have_skills.append(skill.text.strip())
       except:
          print(f"No nice to have data for: {link}")
-         
       return must_have_skills, nice_to_have_skills
+   
+   def job_scrapper_manager(self, jobs_to_download = 0):
+      if jobs_to_download == 0:
+         pages = self.job_pages
+      else:
+         pages = self.job_pages[:jobs_to_download]
+         
+      for job in pages:
+         print(f"Scraping data for: {job[1]}")
+         mandatory, nice_to_have = self.get_job_skills(job[1])
+         mandatory_skills = [job[0], mandatory]
+         nice_to_have_skills = [job[0], nice_to_have]
+         if mandatory_skills not in self.mandatory_skills:
+            self.mandatory_skills.append([job[0], mandatory])
+         if nice_to_have_skills not in self.nice_to_have_skills:
+            self.nice_to_have_skills.append([job[0], nice_to_have])
+            
+            
+   def create_skills_df(self):
+      self.mandatory_df = pd.DataFrame(self.mandatory_skills, columns=["company--title", "skill"])
+      self.mandatory_df = self.mandatory_df.explode("skill")
+      self.nice_to_have_df = pd.DataFrame(self.nice_to_have_skills, columns=["company--title", "skill"])
+      self.nice_to_have_df = self.nice_to_have_df.explode("skill")
+      
+      
+   def skills_to_csv(self, mandatory_csv = "mandatory_skills.csv", nice_to_have_csv = "optional_skills.csv", separator= ";"):
+      self.mandatory_df.to_csv(mandatory_csv, sep=separator)
+      self.nice_to_have_df.to_csv(nice_to_have_csv,sep=separator)
