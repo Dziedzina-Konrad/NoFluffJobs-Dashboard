c67cc46d7bbc361f5530db809266e71253acb61b
Konrad Dziedzina <dziedzina.konrad@gmail.com>
2023-09-17 17:31:25 +0200

    Improving readability and save points of downloaded data

--- scrapper.py
+++ scrapper.py
@@ -1,9 +1,10 @@
+import time
 import pandas as pd
 import requests,json, threading
 from bs4 import BeautifulSoup
 
 class NoFluffJobs():
-   """Class for scrapping and transform data for BI purpose."""
+   """Class for scrapping skills information of every jobs or unique jobs*"""
    def __init__(self):
       """url - all jobs on the portal\n
       job_url - first part of link to job details \n
@@ -28,7 +29,14 @@ class NoFluffJobs():
       self.nice_to_have_skills = []
    
    
-   def job_scrapper_manager(self, jobs_to_download = 0, unique = True):
+   def job_scrapper_manager(self, jobs_to_download = 0, unique = True, save_point = 200):
+      """Func for managment of scraping process.
+
+      Args:
+          jobs_to_download (int, optional): Parametr for how many jobs skills you want download. Defaults to 0.
+          unique (bool, optional): Change if you want download all jobs offerts, not only for unique keys.. Defaults to True.
+          save_point (int, optional): Determinate which itteration do save point in test folder. Defaults to 200.
+      """
       if unique:
          pages = self.unique_jobs
       else:
@@ -48,6 +56,9 @@ class NoFluffJobs():
             self.mandatory_skills.append([job[0], mandatory])
          if nice_to_have_skills not in self.nice_to_have_skills:
             self.nice_to_have_skills.append([job[0], nice_to_have])
+         if(cnt % save_point == 0):
+            self.create_skills_df()
+            self.skills_to_csv("test\\mandatory_safe_point.csv", "test\\optional_safe_point.csv")
          cnt += 1
             
             
@@ -67,6 +78,7 @@ class NoFluffJobs():
          for skill in must_have_section.find_all("li"):
             must_have_skills.append(skill.text.strip())
       except:
+         time.sleep(10)
          soup = BeautifulSoup(requests.get(link).text, "html5lib")
          must_have_section = soup.find("section", class_="d-block")
          for skill in must_have_section.find_all("li"):
@@ -89,5 +101,12 @@ class NoFluffJobs():
       
       
    def skills_to_csv(self, mandatory_csv = "mandatory_skills.csv", nice_to_have_csv = "optional_skills.csv", separator= ";"):
+      """Saving data of skills to files.
+
+      Args:
+          mandatory_csv (str, optional): file name for mandatory dataframe. Defaults to "mandatory_skills.csv".
+          nice_to_have_csv (str, optional): file name gor optional/nice to have datarframe. Defaults to "optional_skills.csv".
+          separator (str, optional): Seperator that is used for creating files. Defaults to ";".
+      """
       self.mandatory_df.to_csv(mandatory_csv, sep=separator)
       self.nice_to_have_df.to_csv(nice_to_have_csv,sep=separator)
