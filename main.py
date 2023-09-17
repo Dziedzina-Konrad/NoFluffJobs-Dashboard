4c3a9c2fc57fcc48b70143574627825118eb6266
Konrad Dziedzina <dziedzina.konrad@gmail.com>
2023-09-17 20:30:01 +0200

    Try to fix bug with problematics pages

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from scrapper import NoFluffJobs
 
 nfj = NoFluffJobs()
 print(f"{len(nfj.unique_jobs)}: {len(nfj.job_pages)}")
-nfj.job_scrapper_manager()
+nfj.load_saved_files()
+print(nfj.mandatory_skills)
+nfj.job_scrapper_manager(skip_loaded_data=True)
 print(nfj.mandatory_skills)
 print(nfj.nice_to_have_skills)
 nfj.create_skills_df()
--- scrapper.py
+++ scrapper.py
@@ -18,6 +18,7 @@ class NoFluffJobs():
       self.job_pages = [] # List of company-title and url
       self.unique_keys = []
       self.unique_jobs = []
+      self.loaded_keys = []
       
       for job in nfj_jobs:
          job_key = job["name"].strip() + "--" + job["title"].strip()
@@ -29,7 +30,7 @@ class NoFluffJobs():
       self.nice_to_have_skills = []
    
    
-   def job_scrapper_manager(self, jobs_to_download = 0, unique = True, save_point = 200):
+   def job_scrapper_manager(self, jobs_to_download = 0, unique = True, save_point = 100, skip_loaded_data = False):
       """Func for managment of scraping process.
 
       Args:
@@ -37,6 +38,7 @@ class NoFluffJobs():
           unique (bool, optional): Change if you want download all jobs offerts, not only for unique keys.. Defaults to True.
           save_point (int, optional): Determinate which itteration do save point in test folder. Defaults to 200.
       """
+      # Check which data will be loaded
       if unique:
          pages = self.unique_jobs
       else:
@@ -48,17 +50,30 @@ class NoFluffJobs():
       cnt = 1
       cnt_all = len(pages)
       for job in pages:
+         # Check if data are loaded.
+         
+         has_data = job[0] in self.loaded_keys
+         if has_data & skip_loaded_data:
+            print(f"Skipped {job[0]} data")
+            cnt += 1
+            continue
+         
          print(f"Scraping data for: {job[1]} \t {cnt}/{cnt_all}")
          mandatory, nice_to_have = self.get_job_skills(job[1])
          mandatory_skills = [job[0], mandatory]
          nice_to_have_skills = [job[0], nice_to_have]
+         
+         # Preventing duplicates
          if mandatory_skills not in self.mandatory_skills:
             self.mandatory_skills.append([job[0], mandatory])
          if nice_to_have_skills not in self.nice_to_have_skills:
             self.nice_to_have_skills.append([job[0], nice_to_have])
+            
+         # 
          if(cnt % save_point == 0):
             self.create_skills_df()
             self.skills_to_csv("test\\mandatory_safe_point.csv", "test\\optional_safe_point.csv")
+            print("Saved data")
          cnt += 1
             
             
@@ -71,13 +86,16 @@ class NoFluffJobs():
       Returns:
           array: Return 2 arrays, first with mandatory skills, second with optional skills.
       """
-      soup = BeautifulSoup(requests.get(link).text, "html5lib")
-      must_have_section = soup.find("section", class_="d-block")
+      request = requests.get(link)
+      soup = BeautifulSoup(request.text, "html5lib")
+      must_have_section = soup.find("section", {"class": "d-block"})
       must_have_skills = []
       try:
-         for skill in must_have_section.find_all("li"):
+         skill_elements =  must_have_section.find_all("li")
+         for skill in skill_elements:
             must_have_skills.append(skill.text.strip())
       except:
+         print(soup.find_all("li"))
          time.sleep(10)
          soup = BeautifulSoup(requests.get(link).text, "html5lib")
          must_have_section = soup.find("section", class_="d-block")
@@ -110,3 +128,25 @@ class NoFluffJobs():
       """
       self.mandatory_df.to_csv(mandatory_csv, sep=separator)
       self.nice_to_have_df.to_csv(nice_to_have_csv,sep=separator)
+   
+   
+   def load_saved_files(self, mandatory_csv="test\\mandatory_safe_point.csv", nice_to_have_csv="test\\optional_safe_point.csv", sep=";"):
+      print("Loading mandatory data.")
+      mandatory = pd.read_csv(mandatory_csv, sep=sep , index_col = False)
+      mandatory = mandatory.groupby("company--title").agg({"skill":list})
+      mandatory_array = []
+      for row in mandatory.iterrows():
+         row_one = row[1].values.tolist()
+         print(row_one)
+         mandatory_array.append([row[0], row_one])
+         self.loaded_keys.append(row[0])
+      self.mandatory_skills = mandatory_array
+      
+      print("Loading nice to have data.")
+      optional = pd.read_csv(nice_to_have_csv, sep=sep , index_col = False)
+      optional = optional.groupby("company--title").agg({"skill":list})
+      optional_array = []
+      for row in optional.iterrows():
+         optional_array.append([row[0], row[1]])
+      self.nice_to_have_skills = optional_array
+      print("Loading saved files completed.")
